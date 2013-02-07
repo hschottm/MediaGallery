f215e2f126630b4f740911ab9e2719c0b766ac3a
Helmut Schottmüller <helmut.schottmueller@me.com>
2013-02-07 09:10:30 +0100

    Added png images for ILIAS 4.3.x compatibility

--- plugin.php
+++ plugin.php
@@ -4,13 +4,13 @@
 $id = "xmg";
  
 // code version; must be changed for all code changes
-define('xmg_version', '1.0.5');
+define('xmg_version', '1.0.6');
 $version = xmg_version;
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.2.0";
-$ilias_max_version = "4.2.999";
+$ilias_max_version = "4.3.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Helmut Schottmüller";
